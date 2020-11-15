c00b1b48068b597c6137db1eb373dce5ba79f226
Natatashkin <cs070283sng@gmail.com>
2020-11-15 18:55:02 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
+
 import './styles.css';
+import { alert, Stack } from '@pnotify/core';
+
+const debounce = require('lodash.debounce');
+
+import FetchCountries from './js/fetchCountries';
+import countryListTpl from './templates/countries-list.hbs';
+import countryInfoTpl from './templates/country-info.hbs';
+import { notification } from './js/notify';
 import AppendMarkup from './js/appendMarkup';
 
-const appendMarkup = new AppendMarkup({
-    inputElement: '.js-search',
-    countriesBlock: 'countries'
+
+
+const renderMarkup = new AppendMarkup({
+  inputElement: '.js-search',
+  countriesBlock: '.countries'
 })
-appendMarkup.init();
+
+renderMarkup.init();
--- src/js/appendMarkup.js
+++ src/js/appendMarkup.js
@@ -2,6 +2,7 @@ import FetchCountries from './fetchCountries';
 import countryListTpl from '../templates/countries-list.hbs';
 import countryInfoTpl from '../templates/country-info.hbs';
 import { notification } from './notify';
+
 const debounce = require('lodash.debounce');
 
 export default class AppendMarkup {
@@ -12,59 +13,79 @@ export default class AppendMarkup {
         }
        this.init();
     }
+
     init() {
         this.debounce = debounce(this.onSearch.bind(this), 500);
         this.refs.input.addEventListener('input', this.debounce);
     }
+
     onSearch(event) {
         const fetchCountries = new FetchCountries();
         this.clearCountriesMarkUp.apply(this);
         fetchCountries.query = event.target.value.trim();
-        console.log(fetchCountries.query);
+    
+
         if (fetchCountries.query === '') {
+            
             this.clearInputField.bind(this);
             notification('error', "Enter country name!");
             return;
         }
+
         fetchCountries.fetchCountriesByName()
             .then(this.appendCountriesMarkUp.bind(this))
             .then(this.clearInputField.bind(this))
             .catch(this.onCatchError.bind(this))
     }
+
     appendCountriesMarkUp(countries) {
 		
         const { length } = countries;
+
 		this.clearCountriesMarkUp();
 		
         if (length < 2) {
             this.showContryInfo(countries);
             return;
         }
+        
         if (length >= 2 && length <= 10) {
             this.clearCountriesMarkUp.apply(this);
             this.showCountryList(countries);
             return;
         }
+        
         if (length > 10) {
             notification('error', "Too mahy matches found. Please enter a more specific query!");
             return;
         }
     }
+
     showContryInfo(countries) {
         this.refs.countriesMarkup.insertAdjacentHTML('beforeend', countryInfoTpl(countries));
     }
+
     showCountryList(countries) {
         this.refs.countriesMarkup.insertAdjacentHTML('beforeend', countryListTpl(countries));
     }
+
     clearCountriesMarkUp() {
         return this.refs.countriesMarkup.innerHTML= '';
     }
+
     clearInputField() {
         this.refs.input.value = '';
     }
+
     onCatchError() {
     this.clearInputField();
         notification('error', "Enter country name!");
         return;
     }
+
+}
+
+async function fetchStates () {
+	const states = await fetch( url ).then( res => res.json())
+	return states;
 }
