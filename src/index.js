080346387228cd85e3cc3effa135affce9b48643
Natatashkin <cs070283sng@gmail.com>
2020-11-15 19:17:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-
 import './styles.css';
-import { alert, Stack } from '@pnotify/core';
-
-const debounce = require('lodash.debounce');
-
-import FetchCountries from './js/fetchCountries';
-import countryListTpl from './templates/countries-list.hbs';
-import countryInfoTpl from './templates/country-info.hbs';
-import { notification } from './js/notify';
 import AppendMarkup from './js/appendMarkup';
-
-
 
 const renderMarkup = new AppendMarkup({
   inputElement: '.js-search',
